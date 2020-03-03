a761ad93266f1dba2e678fdcebdbc912ba9be237
Doug Durham <ddurham@dontpaniclabs.com>
2020-03-03 13:34:27 -0600

    Added retunr values to allow the project to build

--- SummarizeTempsObjects/SummarizeTempsObjects/TempCalculationEngine.cs
+++ SummarizeTempsObjects/SummarizeTempsObjects/TempCalculationEngine.cs
@@ -16,26 +16,31 @@ namespace SummarizeTempsObjects
         public int AverageTemperature()
         {
             // add logic to calculate the average
+            return 0; // <-- Replace this with the actual average
         }
 
         public int TempsAboveThreshold(int threshold)
         {
             // add logic to calculate temps above the threshold
+            return 0; // <-- Replace this with the number
         }
 
         public int TempsBelowThreshold(int threshold)
         {
             // add logic to calculate and return temps below the threshold
+            return 0; // <-- Replace this with the number
         }
 
         public int TempsAtThreshold(int threshold)
         {
             // add logic to calculate and return temps at the threshold
+            return 0; // <-- Replace this with the number
         }
 
         public int NumberOfTemperatures()
         {
             // add logic to return the number of temparature readings
+            return 0; // <-- Replace this with the number
         }
     }
 }
